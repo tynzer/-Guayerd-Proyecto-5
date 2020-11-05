a413ba17a874f98e21a8dbef81b6f88cca21c32d
Tynzer <martinsola119@gmail.com>
2020-11-05 19:55:36 -0300

    porcentaje mostrando

--- src/Components/ItemProducto.js
+++ src/Components/ItemProducto.js
@@ -3,6 +3,19 @@ import React, { Component } from 'react';
 class ItemProducto extends Component {
     render() {
 
+
+/* 
+priceOff = (producto) => {
+
+} 
+ */
+
+const styleOff = {
+    color: "white",
+    size: "15px",
+    padding: "10px",
+  };
+
         return (
             <div>
                 {this.props.productList.map((producto) => {
@@ -12,9 +25,9 @@ class ItemProducto extends Component {
                             <div className="flexbox-productos">
                                 <h2>{producto.title}</h2>
                                 <p>{producto.description}.</p>
-                                <p>{producto.discountPrice ? <><span id="precio"><s> Precio: ${producto.price} ${producto.currency}</s></span>
-                                <br></br><span id="descuento"><b> Precio de descuento: ${producto.discountPrice} ${producto.currency}</b></span></> :
-                                   <> Precio: ${producto.price} ${producto.currency}</>}
+                    <p>{producto.discountPrice ? <><span id="precio"><s> Precio: ${producto.price} {producto.currency}</s></span> <span style ={styleOff}>  {Math.round(100 - ( producto.discountPrice  / producto.price * 100 ) ).toFixed(2)} % OFF</span> 
+                                <br></br><span id="descuento" style={{ fontSize: 18 }}><b> Precio de descuento: ${producto.discountPrice} {producto.currency}</b></span></> :
+                                   <> Precio: ${producto.price} {producto.currency}</>}
                                     <br></br>
                              Stock: {producto.inStock}
                                 </p>
