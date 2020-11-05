d73aad4191a55ab198dd96ab849fa2107de8bb37
Ernesto <ernesto@gmail.com>
2020-11-05 15:42:49 -0300

    4

--- Components/Header.js
+++ Components/Header.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="header">
-            <div className="container name-nav-list"><h1 className="main-text">Guayerd Bikes</h1>
-                <nav className="navigation">        
+            <div>
+                <header className="header">
+                <div className="container name-nav-list"><h1 className="main-text">Guayerd Bikes</h1>
+                    <nav className="navigation">        
                     <ul>
                         <li><a href="index.html">Home</a></li>
                         <li><a href="productos.html">Productos</a></li>
                         <li><a href="contacto.html">Contacto</a></li>
                     </ul>   
-                </nav>
+                    </nav>
+                </div>
+                </header>
             </div>
-            </header>
         );
     }
 }
