aba05c14b8bc257644f64f9ad88b7c6b5af638b4
Tynzer <martinsola119@gmail.com>
2020-11-06 21:31:50 -0300

    app3 aliminado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 /* import App from './App2'; */
-import App from './App3';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
